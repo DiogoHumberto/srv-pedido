3988f303349edf19644a0aef8275bda3818f8854
Diogo Humberto Vitório Dal Bosco <diogo.bosco@solutis.com.br>
2024-04-08 13:48:43 -0300

    projeto base

---
+++ src/main/java/com/study/java/srvpedido/SrvPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.study.java.srvpedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/java/srvpedido/SrvPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.java.srvpedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
